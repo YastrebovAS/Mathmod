84a2e18b1a2aed669b4fb68d594cf49e960cc7c5
Александр Ястребов <sasha_ya@outlook.com>
2024-04-04 22:03:53 +0300

    Initial commit

---
+++ mathmod/main/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+import pandas as pd
+import numpy as np
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'main/index.html')
+
+def practice(request):
+    if request.method == 'POST':
+        dict_of_post = dict(request._post)
+        print(dict_of_post)
+        r = float(dict_of_post['reactivity'][0])
+        beta = float(dict_of_post['beta'][0])
+        step = float(dict_of_post['step'][0])
+        lambd = float(dict_of_post['lambda'][0])
+        l = float(dict_of_post['average_lifetime'][0])
+        Colunms = ['Время', 'Аналитика', 'Р-К метод', 'к1', 'к2', 'к3', 'к4', 'dN', 'Соотн', 'm1', 'm2', 'm3', 'm4',
+                   'dC']
+        time, analytics, PK, k1, k2, k3, k4, dN, sootn, m1, m2, m3, m4, dC = [], [], [], [], [], [], [], [], [], [], [], [], [], []
+        for i in range(0, 99):
+            time.append(step * i)
+            if i == 0:
+                analytics.append(1)
+                sootn.append(1)
+                PK.append(1)
+            else:
+                analytics.append((1 / (1 - r)) * np.exp(lambd * r * time[i] / (1 - r)) - (r / (1 - r)) * np.exp(
+                    (-1 / l) * beta * (1 - r) * time[i]))
+                PK.append(PK[i - 1] + dN[i - 1])
+                sootn.append(sootn[i - 1] + dC[i - 1])
+            m1.append(lambd * (PK[i] - sootn[i]) * step)
+            k1.append(((1 / l) * (beta * (r - 1)) * PK[i] + (beta / l) * sootn[i]) * step)
+            m2.append(lambd * ((PK[i] + k1[i] / 2) - (sootn[i] + m1[i] / 2)) * step)
+            k2.append(((1 / l) * (beta * (r - 1)) * (PK[i] + k1[i] / 2) + (beta / l) * (sootn[i] + m1[i] / 2)) * step)
+            m3.append(lambd * ((PK[i] + k2[i] / 2) - (sootn[i] + m2[i] / 2)) * step)
+            k3.append(((1 / l) * (beta * (r - 1)) * (PK[i] + k2[i] / 2) + (beta / l) * (sootn[i] + m2[i] / 2)) * step)
+            m4.append(lambd * ((PK[i] + k3[i]) - (sootn[i] + m3[i])) * step)
+            k4.append(((1 / l) * (beta * (r - 1)) * (PK[i] + k3[i]) + (beta / l) * (sootn[i] + m3[i])) * step)
+            dN.append((k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6)
+            dC.append((m1[i] + 2 * m2[i] + 2 * m3[i] + m4[i]) / 6)
+
+        numbers = [time, analytics, PK, k1, k2, k3, k4, dN, sootn, m1, m2, m3, m4, dC]
+        data = {}
+        for i in range(0, 14):
+            data[Colunms[i]] = numbers[i]
+        df = pd.DataFrame(data)
+    reactivity = 'reactivity'
+    beta = 'beta'
+    return render(request,'main/practice.html', {'r': reactivity, 'b': beta})
