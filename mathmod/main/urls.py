618dbd8579022feae813ad6638f723287c8ab613
Yastrebov.A.S <70572775+YastrebovAS@users.noreply.github.com>
2024-04-05 11:05:31 +0300

    Update urls.py

--- mathmod/main/urls.py
+++ mathmod/main/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('practice', views.practice, name = 'practice')
+    path('practice', views.practice, name = 'practice'),
+    path('theory', views.theory, name = 'theory')
 ]
